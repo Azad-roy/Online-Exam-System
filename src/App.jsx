8661cdb04c36e89aa0155a7151dfd1601532f1b1
Azad-roy <azadr1233@gmail.com>
2025-10-17 18:12:39 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthPages from "./component/AuthPages";
 import OnlineExamSystem from "./component/OnlineExamSystem";
-import StudentDashboard from './component/StudentDashboard';
+import StudentsDashboard from './component/StudentsDashboard';
 import ResultPage from './component/ResultPage';
 import AdminPanel from './component/AdminPanel';
 import QuizAttempt from './component/QuizAttempt';
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<OnlineExamSystem />} />
         <Route path="/auth" element={<AuthPages />} />
-        <Route path="/dashboard" element={<StudentDashboard />} />
+        <Route path="/dashboard" element={<StudentsDashboard />} />
         <Route path="/results" element={<ResultPage />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/quiz-attempt" element={<QuizAttempt />} />
